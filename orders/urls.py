78fd54b12ef2e2000c91c1ad8146e4017259eafb
Oliver <Oliver.Hnat@gmail.com>
2021-11-04 20:50:58 +0100

    delted urls

--- orders/urls.py
+++ orders/urls.py
@@ -1,21 +1,7 @@
 from django.urls import path
-from . import views
+#from . import views
 
 app_name = "orders"
 urlpatterns = [
-    # pages
-    path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("r/<int:r_id>", views.r_info, name="r_info"),
-    path("h/<int:h_id>", views.h_info, name="h_info"),
-    path("material", views.material, name="material"),
-    
 
-    # functions
-    path("done", views.done, name="done"),
-    path("count", views.count, name="count"),
-    path('needMaterial', views.needMaterial, name="needMaterial"),
-    path('getMaterial', views.getMaterial, name="getMaterial"),
-    #path('getMaterial/<str:table>/<int:id>', views.getMaterial, name="getMaterial"),
 ]
