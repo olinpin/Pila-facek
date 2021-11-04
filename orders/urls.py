00f4c216adefd75507a3593b6ef0610f51c012c1
Oliver <Oliver.Hnat@gmail.com>
2021-11-04 19:48:22 +0100

    Commented out urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+#from . import views
 
 app_name = "orders"
 urlpatterns = [
     # pages
-    path("", views.index, name="index"),
+   """ path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("r/<int:r_id>", views.r_info, name="r_info"),
@@ -17,5 +17,5 @@ urlpatterns = [
     path("count", views.count, name="count"),
     path('needMaterial', views.needMaterial, name="needMaterial"),
     path('getMaterial', views.getMaterial, name="getMaterial"),
-    #path('getMaterial/<str:table>/<int:id>', views.getMaterial, name="getMaterial"),
+    #path('getMaterial/<str:table>/<int:id>', views.getMaterial, name="getMaterial"),"""
 ]
