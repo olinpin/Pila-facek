97aa0277451faa2f44fc252ab1411b1b56db3cb9
Oliver <Oliver.Hnat@gmail.com>
2022-04-02 11:33:46 +0200

    Changed the language

--- PilaFacek/settings.py
+++ PilaFacek/settings.py
@@ -131,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us' #cs
+LANGUAGE_CODE = 'cs' #en-us #cs
 
 TIME_ZONE = 'Europe/Berlin'
 
