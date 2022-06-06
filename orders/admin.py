9d5d57b50aef8c53b45be119fcc20317d3e365f5
Oliver <Oliver.Hnat@gmail.com>
2022-06-06 12:19:55 +0200

    Added another action, can now create pdfs with or without pictures

--- orders/admin.py
+++ orders/admin.py
@@ -93,10 +93,10 @@ class HoblovaniAdmin(admin.ModelAdmin):
             order.save()
     do_vyroby_a_material.short_description = "Zaškrtnout do výroby a materiál"
 
-    def createPDF(modeladmin, request, queryset):
+    def create_pdf_with_pictures(modeladmin, request, queryset):
         response = HttpResponse()
         date = datetime.datetime.now().strftime("%Y-%m-%d")
-        response['Content-Disposition'] = f'attachment; filename=hoblovani-{date}.pdf'
+        response['Content-Disposition'] = f'attachment; filename=hoblovani-s-obrazky-{date}.pdf'
         pdfmetrics.registerFont(TTFont('Verdana', 'orders/fonts/verdana/verdana.ttf'))
         style = ParagraphStyle(name="Normal", fontName="Verdana", fontSize=10, leading=12)
         elements = []
@@ -145,7 +145,53 @@ class HoblovaniAdmin(admin.ModelAdmin):
         doc.build(elements)
         return response
     
-    createPDF.short_description = "Vytvořit PDF"
+    def create_pdf_without_pictures(modeladmin, request, queryset):
+        response = HttpResponse()
+        date = datetime.datetime.now().strftime("%Y-%m-%d")
+        response['Content-Disposition'] = f'attachment; filename=hoblovani-bez-obrazku-{date}.pdf'
+        pdfmetrics.registerFont(TTFont('Verdana', 'orders/fonts/verdana/verdana.ttf'))
+        style = ParagraphStyle(name="Normal", fontName="Verdana", fontSize=10, leading=12)
+        elements = []
+        doc = SimpleDocTemplate(response, rightMargin=0, leftMargin=0, topMargin=0.3 * cm, bottomMargin=0, pagesize=landscape(A4))
+        data = [("Zákazník", "Materiál", Paragraph("Požadovaný rozměr", style), "Poznámka", "Kusy", Paragraph("Balení (proklad)", style), "Kvalita", "Impregnace", "Kapování", Paragraph("Místo hoblování", style), "Vyrobit do", "Priorita", "Obrázek",),]
+        images = []
+        names = []
+        for order in queryset:
+            pozadovana_delka = str(order.pozadovana_delka_cislo) + " " + order.pozadovana_delka_jednotky
+            if order.impregnace:
+                impregnace = "Ano"
+            else:
+                impregnace = "Ne"
+            if order.kapovani:
+                kapovani = "Ano"
+            else:
+                kapovani = "Ne"
+            try:
+                img = Image(order.image)
+                img.drawHeight = 18 * cm
+                img.drawWidth = 18 * cm
+                images.append(img)
+                names.append(order.zakaznik)
+            except:
+                pass
+            if order.image:
+                imgMin = Image(order.image)
+                imgMin.drawHeight = doc.width/13
+                imgMin.drawWidth = doc.width/13-1
+                data.append((Paragraph(order.zakaznik, style), Paragraph(order.skladovy_material, style), Paragraph(f"{order.pozadovany_rozmer} x {pozadovana_delka}"), Paragraph(order.poznamka, style), f"{order.ks} {order.jednotky}", Paragraph(order.baleni, style), Paragraph(order.kvalita, style), Paragraph(impregnace), Paragraph(kapovani), Paragraph(order.misto_hoblovani, style), order.pozadovane_datum_vyroby, order.priority, imgMin))
+            else:
+                data.append((Paragraph(order.zakaznik, style), Paragraph(order.skladovy_material, style), Paragraph(f"{order.pozadovany_rozmer} x {pozadovana_delka}"), Paragraph(order.poznamka, style), f"{order.ks} {order.jednotky}", Paragraph(order.baleni, style), Paragraph(order.kvalita, style), Paragraph(impregnace), Paragraph(kapovani), Paragraph(order.misto_hoblovani, style), order.pozadovane_datum_vyroby, order.priority))
+
+
+        table = Table(data, colWidths=doc.width/13-1)
+        table.setStyle(TableStyle([ ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                                    ('BOX', (0, 0), (-1, -1), 0.25, colors.black),]))
+        elements.append(table)
+        doc.build(elements)
+        return response
+    
+    create_pdf_without_pictures.short_description = "Vytvořit PDF bez obrázků"
+    create_pdf_with_pictures.short_description = "Vytvořit PDF s obrázky"
     def button(self, obj):
         return mark_safe('<input type="submit" name="_save" class="default" value="Uložit">')
     button.short_description = 'Uložit'
@@ -163,7 +209,7 @@ class HoblovaniAdmin(admin.ModelAdmin):
     # exclude in the form
     exclude = ("ks_hotovo", "get_material", "get_zbytek", "odpad", )
     # actions which the admin page can do to orders
-    actions = [do_vyroby_a_material, createPDF,]
+    actions = [do_vyroby_a_material, create_pdf_with_pictures, create_pdf_without_pictures, ]
 
     readonly_fields = ("image_preview", "pozadovana_delka", )
     def image_preview(self, obj):
