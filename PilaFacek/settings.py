a9eda1797c3ed27c941fa5ada716a2fac730f589
Oliver Hnát <oliver.hnat@gmail.com>
2022-11-09 09:55:39 +0100

    Changed settings

--- PilaFacek/settings.py
+++ PilaFacek/settings.py
@@ -24,11 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+if DEBUG:
+    SECRET_KEY = "reouwaghsljfhldkghbvsldf"
+else:
+# SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 ALLOWED_HOSTS = ["0.0.0.0", 'https://pila-facek.herokuapp.com', '*', ]
 
